5a456744cbaf6046fe9b266f9769820c1eaaad10
Jay Kim <useless.idler@gmail.com>
2012-06-28 17:57:20 +0900

    change test

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 $version = phpversion();
-echo "<p>Your app is running on PHP version: " . $version . "<br/></p>";
-echo "<p>The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/></p>";
-echo "<p>The client IP is : " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "<br/></p>";
-echo "<p>Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
+echo "<p>Your app is running on PHP version: " . $version . "<br/>";
+echo "The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/>";
+echo "The client IP is : " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "<br/>";
+echo "Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
 ?>
