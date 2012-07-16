7131b4dba7bb5ac459fb16f08af39d8d02fa71e2
Jay Kim <useless.idler@gmail.com>
2012-07-16 18:22:27 +0900

    minor

--- public/task.php
+++ public/task.php
@@ -5,7 +5,7 @@ echo "<br/>";
 echo "doc_root={$doc_root}" . "<br/>";
 echo "http_method={$http_method}" . "<br/>";
 echo "user_agent={$user_agent}" . "<br/>";
-echo "php_ver={$php_ver}" . "<br/>";
+echo "req_time={$req_time}" . "<br/>";
 echo "<br/>";
 echo "php_ver={$php_ver}" . "<br/>";
 echo "cli_ip={$cli_ip}" . "<br/>";
