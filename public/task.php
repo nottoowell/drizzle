99d256c7c0ea35d128268c2cd2dc64152a74a9cc
Jay Kim <useless.idler@gmail.com>
2012-07-16 18:21:18 +0900

    minor

--- public/task.php
+++ public/task.php
@@ -4,7 +4,7 @@ echo "{$app}/{$cmd}/{$xid}" . "<br/>";
 echo "<br/>";
 echo "doc_root={$doc_root}" . "<br/>";
 echo "http_method={$http_method}" . "<br/>";
-echo "user_agent={$ua}" . "<br/>";
+echo "user_agent={$user_agent}" . "<br/>";
 echo "php_ver={$php_ver}" . "<br/>";
 echo "<br/>";
 echo "php_ver={$php_ver}" . "<br/>";
