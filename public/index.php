062bfd4a8237fb90bf2e567138d0130748aab3fd
Jay Kim <useless.idler@gmail.com>
2012-07-16 17:07:18 +0900

    path_info doesn't work : phpinfo() test

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ if (isset($sliced_path_info[0])) $cmd = $sliced_path_info[0];
 if (isset($sliced_path_info[1])) $id = $sliced_path_info[1];
 if (empty($cmd)) {
 	$cmd = 'enter';
+	$id = '';
 }
 
 echo "{$cmd}/{$id}" . "<br/>";
@@ -17,4 +18,6 @@ echo "<p>Your app is running on PHP version: " . $version . "<br/>";
 echo "The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/>";
 echo "The client IP is : " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "<br/>";
 echo "Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
+
+phpinfo();
 ?>
