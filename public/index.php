d0bb0ed175249fb215ba4e69eee4e16f9402fccb
Jay Kim <useless.idler@gmail.com>
2012-07-16 16:53:50 +0900

    path_info test

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+
+if (isset($_SERVER["PATH_INFO"])) $path_info = substr($_SERVER["PATH_INFO"], 1);
+else $path_info = '';
+
+$sliced_path_info = explode('/', $path_info);
+if (isset($sliced_path_info[0])) $cmd = $sliced_path_info[0];
+if (isset($sliced_path_info[1])) $id = $sliced_path_info[1];
+if (empty($cmd)) {
+	$cmd = 'enter';
+}
+
+echo "{$cmd}/{$id}" . "<br/>";
+
 $version = phpversion();
 echo "<p>Your app is running on PHP version: " . $version . "<br/>";
 echo "The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/>";
