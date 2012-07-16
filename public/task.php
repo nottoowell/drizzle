4dd97b238dc954e5a8e62be009e84f453ec2f4d6
Jay Kim <useless.idler@gmail.com>
2012-07-16 22:32:05 +0900

    commands skeleton

--- public/task.php
+++ public/task.php
@@ -10,5 +10,35 @@ echo "<br/>";
 echo "php_ver={$php_ver}" . "<br/>";
 echo "cli_ip={$cli_ip}" . "<br/>";
 echo "tmp_dir={$tmp_dir}" . "<br/>";
+echo "<br/>";
+
+switch ($cmd) {
+	case 'enter':
+		echo "html";
+		break;
+	case 'clist':
+		echo "category list";
+		break;
+	case 'csave':
+		echo "category save";
+		break;
+	case 'cdele':
+		echo "category delete";
+		break;
+	case 'list':
+		echo "task list";
+		break;
+	case 'save':
+		echo "task save";
+		break;
+	case 'move':
+		echo "task move";
+		break;
+	case 'dele':
+		echo "task delete";
+		break;
+	default:
+		break;
+}
 
 ?>
