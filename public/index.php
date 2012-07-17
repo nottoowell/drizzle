70902f5ca91543dbf7281c1fdfb45cf68f208220
Jay Kim <useless.idler@gmail.com>
2012-07-17 15:12:42 +0900

    dev

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 
-if (isset($_SERVER["PATH_INFO"])) $path_info = substr($_SERVER["PATH_INFO"], 1);
-else if (isset($_SERVER["REQUEST_URI"])) {
+require_once('../core/util.php');
+
+if (isset($_SERVER["PATH_INFO"])) {
+	$path_info = substr($_SERVER["PATH_INFO"], 1);
+} else if (isset($_SERVER["REQUEST_URI"])) {
 	$request_uri = substr($_SERVER["REQUEST_URI"], 1);
 	$sliced_request_uri = explode('?', $request_uri);
 	$path_info = $sliced_request_uri[0];
+} else {
+	$path_info = '';
 }
-else $path_info = '';
 
 $sliced_path_info = explode('/', $path_info);
 if (isset($sliced_path_info[0])) $app = $sliced_path_info[0];
@@ -15,28 +19,25 @@ if (isset($sliced_path_info[2])) $xid = $sliced_path_info[2];
 
 if (empty($app)) $app = 'task';
 if (empty($cmd)) {
-	$cmd = 'enter';
+	$cmd = 'html';
+	$xid = '';
+} else if (empty($xid)) {
 	$xid = '';
 }
-else if (empty($xid)) $xid = '';
 
-if (isset($_SERVER["DOCUMENT_ROOT"])) $doc_root = $_SERVER["DOCUMENT_ROOT"];
-else $doc_root = '';
-if (isset($_SERVER["REQUEST_METHOD"])) $http_method = $_SERVER["REQUEST_METHOD"];
-else $http_method = '';
-if (isset($_SERVER["HTTP_USER_AGENT"])) $user_agent = $_SERVER["HTTP_USER_AGENT"];
-else $user_agent = '';
-if (isset($_SERVER["REQUEST_TIME"])) $req_time = $_SERVER["REQUEST_TIME"];
-else $req_time = '';
+$doc_root = isset_or_empty($_SERVER["DOCUMENT_ROOT"]);
+$http_method = isset_or_empty($_SERVER["REQUEST_METHOD"]);
+$user_agent = isset_or_empty($_SERVER["HTTP_USER_AGENT"]);
+$req_time = isset_or_empty($_SERVER["REQUEST_TIME"]);
 
-if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) $cli_ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-else $cli_ip = '';
+$cli_ip = isset_or_empty($_SERVER["HTTP_X_FORWARDED_FOR"]);
 
 $php_ver = phpversion();
 $tmp_dir = sys_get_temp_dir();
 
-if ($app == 'task') require_once('task.php');
-else if ($app == 'note') require_once('note.php');
+#if ($app == 'task') require_once('task/task.handler.php');
+#else if ($app == 'note') require_once('note/note.handler.php');
+#else require_once('core/404.php');
 
-#phpinfo();
+phpinfo();
 ?>
