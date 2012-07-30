2d3afe0339f86fe4785343090c964ac5f1ec84a4
Jay Kim <useless.idler@gmail.com>
2012-07-30 12:59:11 +0900

    dev

--- public/task/task.php
+++ public/task/task.php
@@ -16,8 +16,8 @@ class TaskGroup {
 	static function with_obje($obje) {
 		$group = new TaskGroup();
 		$group->data = $obje;
-		if (isset($this->data->list_id)) {
-			$this->data->list_id = intval($this->data->list_id);
+		if (isset($group->data->list_id)) {
+			$group->data->list_id = intval($group->data->list_id);
 		}
 		return $group;
 	}
